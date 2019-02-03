dd0ea6f5ee5bbbe54b3c5117f62ed84daab207e5
Yiping Deng <yiping.deng.98@gmail.com>
2019-02-03 14:35:17 +0100

    add gitignore, update repo

--- frontend/src/epic/loginEpic.js
+++ frontend/src/epic/loginEpic.js
@@ -1,12 +1,22 @@
 import { ofType } from 'redux-observable';
 import { LOG_IN, IS_LOG_IN } from '../reducer/loginlogoutreducer';
-import { map } from 'rxjs/operators';
+import { map, flatMap, tap } from 'rxjs/operators';
 
 function loginEpic(action$, state$){
     return action$.pipe(
         ofType(LOG_IN),
+        tap(console.log),
+        flatMap(({payload}) => 
+            fetch('/json_login', {
+                headers: {'Content-type': 'application/json'},
+                method: 'POST',
+                body: JSON.stringify(payload),
+                credentials: 'include'
+            })
+        ),
+        tap(console.log),
         map(()=> ({type: IS_LOG_IN}))
-    )
+    );
 }
 
 export default loginEpic;
